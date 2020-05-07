f943d19c0383ed4b3af2c4a6957b26360656190e
Sean <S-G-M@users.noreply.github.com>
2020-05-06 22:07:16 -0700

    Saved dynamic bot memory locations to playerList vector

--- Cheat Suite/aimbot.cpp
+++ Cheat Suite/aimbot.cpp
@@ -8,11 +8,12 @@ float Vector3::vectorMagnitude()
 
 vector<playerent*> fillEntityArray(uintptr_t* local_player_addr, vector<playerent*> playerList)
 {
-	uintptr_t* numPlayersAddr = (uintptr_t*)0x50F500;
-	uintptr_t entityListPtr = (uintptr_t)local_player_addr + 0x4;
-	uintptr_t startEntityList = *(uintptr_t*)entityListPtr + 0x4;
+	uintptr_t* numPlayersAddr = (uintptr_t*)0x50F500;					// Location of # of players in game
+	uintptr_t entityListPtr = (uintptr_t)local_player_addr + 0x4;		// 10F4F8, location of where bots are stored
+	uintptr_t startEntityList = *(uintptr_t*)entityListPtr + 0x4;		// Index 0 is empty, first bot stored at index 1
 	int numPlayers = *numPlayersAddr;
 
+	// For testing, can comment out latner
 	cout << "numPlayersAddr = " << std::hex << numPlayersAddr << endl;
 	cout << "entityListPtr = " << std::hex << entityListPtr << endl;
 	cout << "startEntityList = " << std::hex << startEntityList << endl;
@@ -20,11 +21,15 @@ vector<playerent*> fillEntityArray(uintptr_t* local_player_addr, vector<playeren
 
 	for (int i = 0; i < numPlayers - 1; i++)
 	{
-		uintptr_t currentEntity = startEntityList + 4*i;
-		cout << "currentEntity = " << std::hex << currentEntity << endl;
-		playerList.push_back((playerent*)currentEntity);
-	}
+		// Increment memory address, storing dynamically allocated bot memory locations into playerList
+		uintptr_t currentEntityList = startEntityList + 4*i;
+		uintptr_t entityMemoryLocation = *(uintptr_t*)currentEntityList;
+		playerList.push_back((playerent*)entityMemoryLocation);
 
+		// For testing
+		cout << "currentEntityList = " << std::hex << currentEntityList << endl;
+		cout << "entityMemoryLocation = " << std::hex << entityMemoryLocation << endl << endl;
+	}
 
 	return playerList;
 }
